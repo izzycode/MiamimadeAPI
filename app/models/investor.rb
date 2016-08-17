8722c6f6cdfda814100f3644f6ba956476664f0f
Israel Martinez 'IzzyCode <marcemc@yahoo.com>
2016-08-17 14:03:56 -0400

    Remove enum with verticals from Startup and Investor models

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -14,31 +14,4 @@ class Investor < ApplicationRecord
       [investor.latitude, investor.longitude]
     end
   end
-
-
-  enum vertical: ["Advertising", "Apps", "Biotechnology", "E-Commerce", "Community and lifestyle", "Consumer Electronics", "Content and Publishing","Data","Education","Fashion","Finance","Food and Beverage","Government and Military","Hardware","Healthcare","Internet Services","Marketing","Media and Entertainment","Mobile","Pharmaceuticals","Software","Real-estate"]
-
 end
-
-# Advertising 0
-# Apps 1
-# Biotechnology 2
-# Commerce and Shopping 3
-# Community and Lifestyle 4
-# Consumer Electronics 5
-# Content and Publishing 6
-# Data and Analytics 7
-# Education 8
-# Fashion 9
-# Financial Services 10
-# Food and Beverage 11
-# Government and Military 12
-# Hardware 13
-# Healthcare 14
-# Internet Services 15
-# Manufacturing 16
-# Media and Entertainment 17
-# Mobile 18
-# Pharmaceuticals 19
-# Software 20
-# Real Estate 21
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -16,29 +16,4 @@ class Startup < ApplicationRecord
       [startup.latitude, investor.longitude]
     end
   end
-
 end
-
-# enum vertical: ['Advertising', "Apps", "Biotechnology", "E-commerce", "Community and lifestyle", "Consumer Electronics", "Content and Publishing","Data","Education","Fashion","Finance","Food and Beverage","Government and Military","Hardware","Healthcare","Internet Services","Marketing","Media and entertainment","Mobile","Pharmaceuticals","Software","Real-estate"]
-# Advertising 0
-# Apps 1
-# Biotechnology 2
-# E-commerce and Shopping 3
-# Community and Lifestyle 4
-# Consumer Electronics 5
-# Content and Publishing 6
-# Data and Analytics 7
-# Education 8
-# Fashion 9
-# Financial Services 10
-# Food and Beverage 11
-# Government and Military 12
-# Hardware 13
-# Healthcare 14
-# Internet Services 15
-# Marketing 16
-# Media and Entertainment 17
-# Mobile 18
-# Pharmaceuticals 19
-# Software 20
-# Real Estate 21
